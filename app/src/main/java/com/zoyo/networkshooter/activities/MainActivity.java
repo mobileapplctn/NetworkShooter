dd80fd346ea7bcc428f9ac9d0858e5f0816695b0
Gokulraj <gokulraj.arunachalam@solverminds.com>
2020-05-23 18:35:01 +0530

    Initial Commit

---
+++ app/src/main/java/com/zoyo/networkshooter/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zoyo.networkshooter.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zoyo.networkshooter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
